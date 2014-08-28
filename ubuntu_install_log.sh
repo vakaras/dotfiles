c57d47346f80da946cbb8a30512a5eba02c639fa
Vytautas Astrauskas <vastrauskas@gmail.com>
2014-08-28 11:20:14 +0200

    Added install log for Ubuntu machine.

---
+++ ubuntu_install_log.sh
@@ -0,0 +1,18 @@
+# Install and configure shell.
+sudo apt-get install zsh zsh-doc
+chsh -s $(which zsh)
+
+# Install tmux.
+sudo apt-get install tmux
+
+# Install VIM.
+sudo apt-get install vim 
+
+# Install Git.
+sudo apt-get install git
+
+# Install development prerequisites.
+sudo apt-get install build-essential bzr mercurial libjpeg62-dev ruby-dev gettext subversion libfreetype6-dev python2.7-dev libxslt1-dev libicu-dev ruby python-dev
+
+# Install VirtualBox.
+sudo apt-get install virtualbox-4.3
