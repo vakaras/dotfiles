e24c6adde3bdd198c1c1a2d62c00b989916f7e50
Vytautas Astrauskas <vastrauskas@gmail.com>
2014-08-28 17:46:29 +0200

    Playing around with xmonad config.

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,6 +1,18 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Util.CustomKeys
 
+-- Change mod key from alt to super.
 main = xmonad gnomeConfig
     { modMask = mod4Mask
+      , keys  = customKeys delKeys insKeys
     }
+    where
+      delKeys :: XConfig l -> [(KeyMask, KeySym)]
+      delKeys XConfig {modMask = modm} =
+        -- Delete nothing.
+        []
+      insKeys :: XConfig l -> [((KeyMask, KeySym), X ())]
+      insKeys conf@(XConfig {modMask = modm}) =
+        -- Insert nothing.
+        []
