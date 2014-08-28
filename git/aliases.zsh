ce422ad737db84bdf296dd5da150d71fba19b9c1
Vytautas Astrauskas <vastrauskas@gmail.com>
2014-08-28 13:50:13 +0200

    Updated git aliases.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,13 +7,18 @@ then
 fi
 
 # The rest of my fun git aliases
+alias ga='git add'
+alias gap='git add --patch'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
 alias gd='git diff'
+alias gdc='git diff --cached'
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcm='git commit -m'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias git-remove-ignored='git clean -d -x -f'
