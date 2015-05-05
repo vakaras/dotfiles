1672bcfb4e9e0b2f695caeb548bc28c7ffc91b0c
Vytautas Astrauskas <vastrauskas@gmail.com>
2015-05-05 11:20:45 +0200

    Enabled backward search.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,6 +39,9 @@ setopt complete_aliases
 
 zle -N newtab
 
+# Enable VI mode (man readline).
+set -o vi
+
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
@@ -46,6 +49,5 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+bindkey '^R' history-incremental-search-backward
 
-# Enable VI mode (man readline).
-set -o vi
