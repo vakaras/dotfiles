13087a4fa37fcf8ba3fa5afd5c75cf12c4b48520
Vytautas Astrauskas <vastrauskas@gmail.com>
2016-04-15 18:16:49 +0200

    Updated path for fish.

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,4 @@
 fish_vi_mode
+set PATH "$HOME/.texlive/2015/bin/x86_64-linux" $PATH
+set PATH "$HOME/.dotfiles/bin" $PATH
+set MANPATH "$HOME/.texlive/2015/texmf-dist/doc/man" $MANPATH
--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="$HOME/.texlive/2014/bin/x86_64-linux:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="$HOME/.texlive/2015/bin/x86_64-linux:$HOME/.dotfiles/bin:$PATH"
+export MANPATH="$HOME/.texlive/2015/texmf-dist/doc/man:$MANPATH"
